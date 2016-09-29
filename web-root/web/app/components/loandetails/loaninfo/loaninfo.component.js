1d3d09564a1c014deb991fc00fd240a54adb9361
Ashutosh Mishra <ashutosh2289@gmail.com>
2016-09-29 14:59:51 +0530

    Code formatting for loan info-lint fixes

--- web-root/web/app/components/loandetails/loaninfo/loaninfo.component.js
+++ web-root/web/app/components/loandetails/loaninfo/loaninfo.component.js
@@ -2,58 +2,56 @@
 
 (function() {
 
-    var loanInfoController = function(loanService) {
+	var loanInfoController = function(loanService) {
 
-        var $ctrl = this;
-        $ctrl.enableRateSection = function() {
-            loanService.getCurrentRate().then(function(response) {
-                $ctrl.currentRate = response.data;
-                $ctrl.showRateSection = true;
-            }, function(err){
-            	$ctrl.showRateSection = false;
-            });
-        };
+		var $ctrl = this;
+		$ctrl.enableRateSection = function() {
+			loanService.getCurrentRate().then(function(response) {
+				$ctrl.currentRate = response.data;
+				$ctrl.showRateSection = true;
+			}, function() {
+				$ctrl.showRateSection = false;
+			});
+		};
 
-        $ctrl.onLoanInfoSave = function(form) {
-            if (form.$valid) {
-                //TODO: call service API
-                saveLoanInfoData();
-                loanService.loanAmount = $ctrl.user.loanAmount;
-                $ctrl.openCollateralAccordian();
-            }
-        };
+		$ctrl.onLoanInfoSave = function(form) {
+			if (form.$valid) {
+				//TODO: call service API
+				saveLoanInfoData();
+				loanService.loanAmount = $ctrl.user.loanAmount;
+				$ctrl.openCollateralAccordian();
+			}
+		};
 
-        function saveLoanInfoData() {
+		function saveLoanInfoData() {
 
-            /*var loanInfoData = {
-                firstName: $ctrl.user.firstName,
-                middeleInitial: $ctrl.user.middleName,
-                lastName: $ctrl.user.lastName,
-                emailId: $ctrl.user.email,
-                mobileNumber: $ctrl.user.mobilenumber,
-                loanAmount: $ctrl.user.loanAmount,
-                useofLoan: $ctrl.user.selectedReason.Reason
-            };*/
-            //TODO -- code for posting this data to backend
+			/*var loanInfoData = {
+			    firstName: $ctrl.user.firstName,
+			    middeleInitial: $ctrl.user.middleName,
+			    lastName: $ctrl.user.lastName,
+			    emailId: $ctrl.user.email,
+			    mobileNumber: $ctrl.user.mobilenumber,
+			    loanAmount: $ctrl.user.loanAmount,
+			    useofLoan: $ctrl.user.selectedReason.Reason
+			};*/
+			//TODO -- code for posting this data to backend
+		}
 
-        }
+		$ctrl.showRateSection = false;
 
-        $ctrl.showRateSection = false;
+	};
 
-    };
+	loanInfoController.$inject = [ 'loanService' ];
 
-    loanInfoController.$inject = ['loanService'];
+	var loanInfoConfig = {
+		bindings : {
+			usesOfLoanProceeds : '=',
+			openCollateralAccordian : '&'
+		},
+		templateUrl : 'loandetails/loaninfo/loaninfo.html',
+		controller : loanInfoController
+	};
 
-    var loanInfoConfig = {
-        bindings: {
-            usesOfLoanProceeds: '=',
-            openCollateralAccordian: '&'
-        },
-        templateUrl: 'loandetails/loaninfo/loaninfo.html',
-        controller: loanInfoController
-    };
-
-    angular.module('loandetails').component('loanInfo', loanInfoConfig);
-
+	angular.module('loandetails').component('loanInfo', loanInfoConfig);
 
 })();

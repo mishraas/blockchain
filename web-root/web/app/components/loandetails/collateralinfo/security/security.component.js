250611c74a1a976886a00d88caa7841396120daf
Sumit Kumar 13 <sku315@sapient.com>
2016-10-06 13:58:07 +0530

    Commiting some ui changes and changed username to full name in nav section

--- web-root/web/app/components/loandetails/collateralinfo/security/security.component.js
+++ web-root/web/app/components/loandetails/collateralinfo/security/security.component.js
@@ -17,13 +17,13 @@
         ];
 
         $ctrl.$onInit = function() {
-            $ctrl.loanApprovalFlag = $ctrl.loan.collateralValue >= (0.8*$ctrl.loan.loanAmount);
             if ($ctrl.loan.id) {
                 $ctrl.showPositionFlag = true;
                 $ctrl.securityDetails = {};
                 $ctrl.securityDetails.columns = $ctrl.positionColumns;
                 $ctrl.securityDetails.data = $ctrl.loan.collateralPositions;
                 $ctrl.loan.collateralValue = loanService.calculateTotalCollateralAmount($ctrl.securityDetails.data); 
+                $ctrl.loanApprovalFlag = $ctrl.loan.collateralValue >= (0.8*$ctrl.loan.loanAmount);
                 
             }
         };
